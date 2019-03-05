2b4f6f33e77468133cf156cc6a67fec35aee2a75
ne1s0n <nelson.nazzicari@gmail.com>
2019-03-05 10:44:56 +0100

    updated data storing script so that it is not executed on Rstudio build&reload

--- R/store_data_in_package.R
+++ R/store_data_in_package.R
@@ -16,38 +16,37 @@
 
 # This is an utility script to load a Seurat object from (my) computer
 # into the MTGOsc package. It's here for handyness, repeatibility, and to show
-# the process to the curious investigator
+# the process to the curious investigator. If you want to run the code do a
+# big uncomment below this point.
 
-setwd("~/research/MTGOsc/")
-
-#loading bladder full dataset
-bladder = readRDS('~/research/Seurat2MTGO.notes/dataset5.2/data/Bladder.dataset5.RDS')
-
-#removing the heavy parts of the Seurat object, not used by MTGOsc
-bladder@raw.data = NULL
-bladder@scale.data = NULL
-
-#finding differentially expressed genes for each cluster
-markers = FindAllMarkers(bladder, logfc.threshold = 1)
-table(markers$cluster)
-
-#listing the available clusters, with cell count
-table(bladder@ident)
-
-#subsetting to just three clusters: a big one, a medium one, a small one
-#and subsetting also to differentially expressed genes
-
-#Stromal cell_Dpt high(Bladder) : 651 cells
-#Urothelium(Bladder) : 277 cells
-#Umbrella cell(Bladder) : 50 cells
-target = c('Stromal cell_Dpt high(Bladder)', 'Urothelium(Bladder)', 'Umbrella cell(Bladder)')
-cell.selection = bladder@ident %in% target
-gene.selection = markers$cluster %in% target
-bladder@data = bladder@data[gene.selection,cell.selection]
-bladder@ident = bladder@ident[cell.selection]
-markers = markers[gene.selection,]
-
-#storing in MTGOsc
-devtools::use_data(bladder, markers, overwrite = TRUE)
-
-
+# setwd("~/research/MTGOsc/")
+#
+# #loading bladder full dataset
+# bladder = readRDS('~/research/Seurat2MTGO.notes/dataset5.2/data/Bladder.dataset5.RDS')
+#
+# #removing the heavy parts of the Seurat object, not used by MTGOsc
+# bladder@raw.data = NULL
+# bladder@scale.data = NULL
+#
+# #finding differentially expressed genes for each cluster
+# markers = FindAllMarkers(bladder, logfc.threshold = 1)
+# table(markers$cluster)
+#
+# #listing the available clusters, with cell count
+# table(bladder@ident)
+#
+# #subsetting to just three clusters: a big one, a medium one, a small one
+# #and subsetting also to differentially expressed genes
+#
+# #Stromal cell_Dpt high(Bladder) : 651 cells
+# #Urothelium(Bladder) : 277 cells
+# #Umbrella cell(Bladder) : 50 cells
+# target = c('Stromal cell_Dpt high(Bladder)', 'Urothelium(Bladder)', 'Umbrella cell(Bladder)')
+# cell.selection = bladder@ident %in% target
+# gene.selection = markers$cluster %in% target
+# bladder@data = bladder@data[gene.selection,cell.selection]
+# bladder@ident = bladder@ident[cell.selection]
+# markers = markers[gene.selection,]
+#
+# #storing in MTGOsc
+# devtools::use_data(bladder, markers, overwrite = TRUE)
